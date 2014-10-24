f04e3e77439fe160d4ac49b24ed16d8641ba5bfa
Sam Hathaway <samuel.hathaway@macmillan.com>
2014-10-23 21:04:53 -0400

    start jobs processor in second window of same screen session

--- bootstrap/start.sh
+++ bootstrap/start.sh
@@ -4,6 +4,10 @@
 PATH=/usr/local/bin:"$PATH"; export PATH
 NODE_PATH=/usr/lib/nodejs:/usr/lib/node_modules:/usr/share/javascript; export NODE_PATH
 
-echo -n "Starting Canvas LMS in a screen session: canvas-lms..."
-screen -d -m -S canvas-lms sh -c "cd $HOME/canvas && bundle exec rails server"
+echo -n "Staring Canvas LMS: "
+screen -S canvas -dm -t lms sh -c "cd $HOME/canvas && bundle exec rails server"
+echo " done."
+
+echo -n "Starting Canvas background job processor: "
+screen -S canvas -X screen -t jobs sh -c "cd $HOME/canvas && bundle exec script/delayed_job run"
 echo " done."
